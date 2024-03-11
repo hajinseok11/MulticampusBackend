15f2e999351db4ad0420e3a1fbde87dd8a63772a
Hajinseok <ha661001@nate.com>
2024-03-11 21:05:23 +0900

    front

---
+++ clientweb/src/main/webapp/common/js/exam.js
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+function test1(){
+		alert("test1함수");
+}
+function test2(){
+	myvalue = confirm("삭제하시겠어요?");
+	alert(myvalue);
+	console.log(myvalue);
+}
+function test3(){
+	mysubject = prompt("좋아하는 언어는?(사용자에게 입력 받는 대화 상자)");
+	alert(mysubject);
+	console.log(mysubject);
+}
+ 
